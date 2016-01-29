5e3a95cd5d05ca7f7e53c83e7f521bcbbf6a8efa
takezomiyamoto <takezo.m.1987@gmail.com>
2016-01-29 18:19:15 +0000

    add profile column in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   
+  # edit, update の時は, Login しているかを先に確認する
+  before_action :logged_in_user, only:[:edit, :update]
+  
+  
   def show
     @user = User.find(params[:id])
   end
@@ -20,11 +24,17 @@ class UsersController < ApplicationController
   end
   
   def edit
-    
+    @user = User.find(params[:id])
   end
   
   def update
-    current_user.update(user_params)
+    @user = User.find(params[:id])
+    if @user.update(edit_user_params)
+      flash[:success] = "Your profile was updated!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
   private
@@ -33,4 +43,15 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
+  def edit_user_params
+    params.require(:user).permit(:name, :email, :origin, :profile)
+  end
+  
+  #ログイン中か確認 
+  def logged_in_user
+    unless logged_in
+      flash[:danger] = "Please log in first."
+      redirect_to login_path
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 225 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-    
+    validates :origin, presence: true, length: { maximum: 30 }
+    validates :profile, presence: false, length: { maximum: 150 }
     has_secure_password
 end
