8f3bedec89a8c2eceb29d18a0ab20531fc2d7219
takezomiyamoto <takezo.m.1987@gmail.com>
2016-02-06 13:39:49 +0000

    add home view timeline

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    
+    if logged_in?
     # ログインしている場合は、新しいMicropostクラスのインスタンスをuser_idを紐付けた状態で初期化
-    @micropost = current_user.microposts.build if logged_in?
+    @micropost = current_user.microposts.build 
+    @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+    end
   end
+  
 end
 
 # current_user.microposts.buildは、Micropost.new(user_id: current_user.id)と同じです。
--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,9 @@ class User < ActiveRecord::Base
         following_users.include?(other_user)
       end
       
+    def feed_items #user_idがフォローしているユーザーと自分のつぶやきを取得
+      Micropost.where(user_id: following_user_ids + [self.id])
+    end
     
       
 end
